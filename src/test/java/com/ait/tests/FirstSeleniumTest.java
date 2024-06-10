04100667f0e9f9e4989091733d1eba4ea233f9ea
tatjana <t.voronko@gmail.com>
2024-06-10 21:04:54 +0200

    First commit

---
+++ src/test/java/com/ait/tests/FirstSeleniumTest.java
@@ -0,0 +1,37 @@
+package com.ait.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class FirstSeleniumTest {
+
+    WebDriver driver;
+
+    //before - setUp
+    @BeforeMethod
+    public void setUp(){
+     driver = new ChromeDriver();
+     driver.get("https://www.google.com/");
+
+     driver.navigate().to("https://www.google.com/"); //with history
+      driver.navigate().back();
+     driver.navigate().refresh();
+    }
+
+    //tests
+
+    @Test
+    public void openGoogle(){
+        System.out.println("Google opened!");
+    }
+    //after - tearDown
+    @AfterMethod
+    public void tearDown(){
+        //interview question
+        driver.quit(); //close all tabs & browser
+//        driver.close(); //close only one tab ( if only one tab -> close browser)
+    }
+}
