a49063450162e18fa4df054f25bb2cb079dc4a70
tatjana <t.voronko@gmail.com>
2024-06-14 15:59:37 +0200

    Homework #4

--- src/test/java/com/ait/tests/DemoWebShopTest.java
+++ src/test/java/com/ait/tests/DemoWebShopTest.java
@@ -1,42 +1,20 @@
 package com.ait.tests;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
 import java.util.List;
 
-public class DemoWebShopTest {
-    WebDriver driver;
-
-    @BeforeMethod
-    public void setUp(){
-        driver = new ChromeDriver();
-        driver.get("https://demowebshop.tricentis.com/");
-
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-    }
+public class DemoWebShopTest extends TestBase{
 
     @Test
     public void openDemoWebShop(){
         System.out.println("DemoWebShop opened!");
     }
 
-    @AfterMethod
-    public void tearDown(){
-        driver.quit();
-    }
 
-
-
-//    HW #2======================================================================
+    //    HW #2======================================================================
     @Test
     public void findElementsByTagName() {
 //        find elements by tag
