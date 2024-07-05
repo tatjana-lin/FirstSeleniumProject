ea90751ec5a5effb3b7f38e4341eceee6b383d53
tatjana <t.voronko@gmail.com>
2024-07-05 12:26:04 +0200

    Homework #15 update

--- src/main/java/com.theintermet.pages/JQueryUIMenusPage.java
+++ src/main/java/com.theintermet.pages/JQueryUIMenusPage.java
@@ -6,23 +6,25 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.testng.Assert;
 
+import java.io.File;
+
 public class JQueryUIMenusPage extends BasePage {
     public JQueryUIMenusPage(WebDriver driver) {
         super(driver);
     }
 
     @FindBy(id = "ui-id-3")
-    WebElement mainItem2;
+    WebElement enabled;
 
     @FindBy(css = "#ui-id-8>a")
-    WebElement subMenuItem2;
+    WebElement back;
 
     public JQueryUIMenusPage selectBackToJQuery() {
 
         Actions actions = new Actions(driver);
-        actions.moveToElement(mainItem2).perform();
+        actions.moveToElement(enabled).perform();
         pause(1000);
-        actions.click(subMenuItem2).perform();
+        actions.click(back).perform();
         pause(1000);
 
         return this;
@@ -37,27 +39,62 @@ public class JQueryUIMenusPage extends BasePage {
     }
 
     @FindBy(id = "ui-id-4")
-    WebElement subMenuItem1;
-
+    WebElement download;
+    @FindBy(css = "#ui-id-5>a")
+    WebElement pdf;
     @FindBy(css = "#ui-id-6>a")
-    WebElement subSubMenuItem2;
+    WebElement csv;
+    @FindBy(css = "#ui-id-7>a")
+    WebElement excel;
 
-    public JQueryUIMenusPage selectDownloadsCsvFile() {
-        Actions actions = new Actions(driver);
-        actions.moveToElement(mainItem2).perform();
-        pause(1000);
-        actions.moveToElement(subMenuItem1).perform();
-        pause(1000);
-        actions.click(subSubMenuItem2).perform();
+
+    public JQueryUIMenusPage hoverEnabled() {
+        new Actions(driver).moveToElement(enabled).perform();
         pause(1000);
         return this;
     }
 
-    public JQueryUIMenusPage verifyFilePath(String filePath) {
-        Assert.assertTrue(isFileDownloaded(filePath, "menu.csv"));
+    public JQueryUIMenusPage hoverDownloads() {
+        new Actions(driver).moveToElement(download).perform();
+        pause(1000);
         return this;
     }
 
+    public JQueryUIMenusPage clickPdf() {
+        click(pdf);
+        return this;
+    }
+
+    public JQueryUIMenusPage clickCsv() {
+        click(csv);
+        return this;
+    }
+
+    public JQueryUIMenusPage clickExcel() {
+        click(excel);
+        return this;
+    }
+
+    public JQueryUIMenusPage checkFileDownloaded(String path, String name) {
+        pause(2000);
+        Assert.assertTrue(isFileDownloaded(path,name));
+        pause(1000);
+        return null;
+    }
+
+    public JQueryUIMenusPage removeDownloadedFile(String path, String name) {
+        pause(2000);
+        File dir = new File(path);
+        File[] dirContents = dir.listFiles();
+
+        for (int i = 0; i < dirContents.length; i++) {
+            if (dirContents[i].getName().contains(name)) {
+                dirContents[i].delete();
+                return this;
+            }
+        }
+        return this;
+    }
 
 }
 
--- src/test/java/com.theinternet.tests/JQueryUIMenusTests.java
+++ src/test/java/com.theinternet.tests/JQueryUIMenusTests.java
@@ -2,27 +2,55 @@ package com.theinternet.tests;
 
 import com.theintermet.pages.HomePage;
 import com.theintermet.pages.JQueryUIMenusPage;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class JQueryUIMenusTests extends TestBase{
+    HomePage home;
+    JQueryUIMenusPage jQuery;
+    String path = "C:/Users/Tanya/Downloads";
 
     @BeforeMethod
     public void precondition(){
-        new HomePage(driver).getJQueryUIMenusPage();
+        home = new HomePage(driver);
+        jQuery = new JQueryUIMenusPage(driver);
+        home.getJQueryUIMenusPage();
     }
 
     @Test
     public void backToJqueryUITest(){
-        new JQueryUIMenusPage(driver).selectBackToJQuery()
+        jQuery.selectBackToJQuery()
                 .verifyText("is many things, but one thing");
-
     }
 
     @Test
-    public void downloadCsvFileTest(){
-        new JQueryUIMenusPage(driver).selectDownloadsCsvFile()
-                .verifyFilePath("C:/Users/Tanya/Downloads");
+    public void jQueryUIMenusPdfTest() {
+        jQuery.hoverEnabled()
+                .hoverDownloads()
+                .clickPdf()
+                .checkFileDownloaded(path,"menu.pdf");
+    }
+
+    @Test
+    public void jQueryUIMenusCsvTest() {
+        jQuery.hoverEnabled()
+                .hoverDownloads()
+                .clickCsv()
+                .checkFileDownloaded(path,"menu.csv");
+    }
+
+    @Test
+    public void jQueryUIMenusExcelTest() {
+        jQuery.hoverEnabled()
+                .hoverDownloads()
+                .clickExcel()
+                .checkFileDownloaded(path,"menu.xls");
+    }
+
+    @AfterMethod
+    public void postCondition() {
+        jQuery.removeDownloadedFile(path,"menu");
     }
 
 }
